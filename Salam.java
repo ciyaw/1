889cc41793c66803a027fb8abd283a699cd4c601
ciyaw <140060622+ciyaw@users.noreply.github.com>
2024-09-21 16:25:49 +0800

    Add files via upload

---
+++ Salam.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halo;
+
+/**
+ *
+ * @author USER
+ */
+public class Salam {
+    public static void main(String[] args) {
+        String negara = "Indonesia";
+        int  jmlProv = 38;
+        
+        System.out.println("Halo " + negara);
+        System.out.println("Jumlah provinsi anda adalah " + jmlProv);
+    }
+}
